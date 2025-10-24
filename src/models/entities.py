c7fb9ac728d6c3481084c824fd26e410ee0686b9
estebanArmonica <110997413+estebanArmonica@users.noreply.github.com>
2025-10-24 09:56:59 -0300

    Update entities.py

--- src/models/entities.py
+++ src/models/entities.py
@@ -19,13 +19,6 @@ class DimTiempo:
     created_at: datetime
 
 @dataclass
-class Empresa:
-    id_emp: int
-    nombre: str
-    tipo: Optional[str]
-    created_at: datetime
-
-@dataclass
 class PrecioMarginal:
     id_pr_mrgl: int
     tiempo_id: int
@@ -40,10 +33,11 @@ class RetiroEnergia:
     id_rt: int
     tiempo_id: int
     barra_id: int
-    suministrador_id: int
-    retiro_id: int
+    suministrador: str  
+    retiro: str  
     clave: str
     tipo: str
+    clave_anio_mes: str
     medida_kwh: float
     created_at: datetime
 
@@ -53,7 +47,7 @@ class ContratoFisico:
     tiempo_id: int
     barra_id: int
     clave: str
-    empresa_id: int
+    nom_empresa: str  
     transaccion: str
     kwh: float
     valorizado_clp: float
